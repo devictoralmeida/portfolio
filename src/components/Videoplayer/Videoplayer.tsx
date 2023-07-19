cf61d92a2eb9de2bb1c4f77d6ead77de65fb3366
Victor Emmanuel Melo e Almeida <victoremmanuelmn@gmail.com>
2023-07-19 11:01:10 -0300

    test

--- src/components/Videoplayer/Videoplayer.tsx
+++ src/components/Videoplayer/Videoplayer.tsx
@@ -28,13 +28,12 @@ const VideoPlayer = () => {
   }, []);
 
   return (
-    <div className="player-wrapper" ref={playerRef} style={{height: "350px", width: "350px" }}>
+    <div className="player-wrapper" ref={playerRef} style={{height: "350px", width: "300px" }}>
       {isInView && (
         <ReactPlayer
           className="react-player"
           url="video.MP4"
           width="100%"
-          //height="100%"
           muted={true}
           playing={true}
           loop={true}
