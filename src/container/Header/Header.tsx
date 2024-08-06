8214cf15becfc4efd81f82a240f3b31a2bc04ee4
Victor Almeida <victor.almeida@goldencloud.tech>
2024-08-06 15:10:08 -0300

    update portfolio

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.java, images.spring, images.angular].map(
+        {[images.spring, images.java, images.angular].map(
           (circle, index) => (
             <div className="circle-cmp app__flex" key={`circle-${index}`}>
               <Image src={circle} alt="circle" />
