a5ad2847e08045bbe6871947ca6e21d63a42fa37
Victor Almeida <victor.almeida@goldencloud.tech>
2024-08-06 15:07:59 -0300

    update portfolio

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,21 +1,24 @@
-import email from "../assets/email.png";
+import angular from '../assets/angular.png';
+import circle from "../assets/circle.svg";
 import css from "../assets/css.png";
+import email from "../assets/email.png";
 import git from "../assets/git.png";
 import html from "../assets/html.png";
+import java from '../assets/java.png';
 import javascript from "../assets/javascript.png";
+import logo from '../assets/logo.png';
+import me from "../assets/me.png";
+import node from '../assets/node.png';
+import perfil4 from "../assets/perfil4.png";
 import react from "../assets/react.svg";
 import sass from "../assets/sass.png";
-import typescript from "../assets/typescript.svg";
-import styled from "../assets/styled-components.png";
-import circle from "../assets/circle.svg";
-import perfil4 from "../assets/perfil4.png";
-import bg from "../assets/wp1.jpeg";
-import me from "../assets/me.png";
 import speech from "../assets/speech.gif";
 import speech2 from "../assets/speech2.gif";
-import wpp from '../assets/wpp.svg'
-import logo from '../assets/logo.png';
-import node from '../assets/node.png'
+import spring from '../assets/spring-boot.png';
+import styled from "../assets/styled-components.png";
+import typescript from "../assets/typescript.svg";
+import bg from "../assets/wp1.jpeg";
+import wpp from '../assets/wpp.svg';
 
 const images = {
   wpp,
@@ -36,6 +39,9 @@ const images = {
   typescript,
   circle,
   node,
+  java,
+  spring,
+  angular
 };
 
 export default images;
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.typescript, images.node, images.react].map(
+        {[images.java, images.spring, images.angular].map(
           (circle, index) => (
             <div className="circle-cmp app__flex" key={`circle-${index}`}>
               <Image src={circle} alt="circle" />
